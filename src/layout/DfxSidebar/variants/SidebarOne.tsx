15779eed8fa66983f2fa2f01df1bf4149cd501da
Bhupesh Singh Chauhan <107687641+BhupeshChauhan@users.noreply.github.com>
2024-07-04 19:23:42 +0530

    Update SidebarOne.tsx

--- src/layout/DfxSidebar/variants/SidebarOne.tsx
+++ src/layout/DfxSidebar/variants/SidebarOne.tsx
@@ -92,7 +92,8 @@ export const SidebarOne = ({
       <div
         className={cn(
           'fixed top-4 flex flex-col items-start justify-between h-full flex-1 w-full min-w-[500px]',
-          expanded ? 'mt-[230px]' : 'mt-[130px]'
+          expanded ? 'mt-[230px]' : 'mt-[130px]',
+          expanded ? 'max-w-60' : 'max-w-14'
         )}
       >
         <div
