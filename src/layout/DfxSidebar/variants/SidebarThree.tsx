81dd94f0e8444e4a9066bb13b8c4f86a8dfdb055
BhupeshChauhan <erchauhanbhupesh@gmail.com>
2024-09-02 18:38:13 +0530

    Update SidebarThree.tsx

--- src/layout/DfxSidebar/variants/SidebarThree.tsx
+++ src/layout/DfxSidebar/variants/SidebarThree.tsx
@@ -44,67 +44,79 @@ export const SidebarThree = ({
           : 'relative flex flex-col items-start w-24 px-2 h-screen py-4 overflow-y-auto'
       )}
     >
-      <div className={cn(
-        expanded
-          ? 'bg-white m-2 my-4 shadow-2xl rounded-3xl h-full w-80'
-          : 'bg-white m-2 my-4 shadow-2xl rounded-3xl h-full w-16'
-      )}>
-      {logo}
-      <TypographyComp
-        as={menuType}
-        className={cn(
-          expanded
-            ? 'flex gap-3 items-center mt-6 -mx-2'
-            : 'flex items-center justify-center'
-        )}
-        {...(libraryType === 'react' && {
-          to: profilePath,
-        })}
-        {...(libraryType === 'next' && {
-          href: profilePath,
-        })}
-      >
-        {profileImage}
-        <div className={expanded ? 'block' : 'hidden'}>
-          {profileName && (
-            <h4 className="mx-2 font-medium text-gray-800 dark:text-gray-200">
-              {profileName}
-            </h4>
-          )}
-          {profileDescription && (
-            <p className="mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400">
-              {profileDescription}
-            </p>
-          )}
-        </div>
-      </TypographyComp>
       <div
         className={cn(
-          'fixed top-4 flex flex-col items-start justify-between h-full flex-1 w-full',
-          expanded ? 'mt-[150px]' : 'mt-[100px]',
-          expanded ? 'max-w-60' : 'max-w-14'
+          expanded
+            ? 'bg-white m-2 my-4 shadow-2xl rounded-3xl h-full w-80 flex justify-between flex-col'
+            : 'bg-white m-2 my-4 shadow-2xl rounded-3xl h-full w-16 flex justify-center'
         )}
       >
-        <div
-          className={cn(
-            'absolute h-full w-full',
-            expanded ? 'max-w-60' : 'max-w-14'
-          )}
-        >
-          <MenuList
-            showText={expanded}
-            library={libraryType}
-            variant="basic"
-            menuArrays={menuArrays}
-            type={menuType}
-            linkClassName="w-full"
-            className="w-full"
-            tooltipClassName="bg-white"
-          />
+        <div>
+        <div>
+          {logo}
+          <TypographyComp
+            as={menuType}
+            {...(libraryType === 'react' && {
+              to: profilePath,
+            })}
+            {...(libraryType === 'next' && {
+              href: profilePath,
+            })}
+            className="flex flex-col items-center mt-6 -mx-2"
+          >
+            {profileImage}
+            {profileName && (
+              <h4
+                className={cn(
+                  expanded
+                    ? 'mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200'
+                    : 'hidden'
+                )}
+              >
+                {profileName}
+              </h4>
+            )}
+            {profileDescription && (
+              <p
+                className={cn(
+                  expanded
+                    ? 'mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400'
+                    : 'hidden'
+                )}
+              >
+                {profileDescription}
+              </p>
+            )}
+          </TypographyComp>
         </div>
         <div
           className={cn(
-            'fixed w-full mb-6 z-30 bottom-0 flex items-center justify-center px-4 py-4 mt-5 text-gray-600 transition-colors duration-300 transform dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700',
+            'flex flex-col items-start justify-between flex-1 w-full',
+            expanded ? 'h-[70%]' : 'h-[70%]',
+          )}
+        >
+          <div
+            className={cn(
+              'absolute w-full',
+              expanded ? 'max-w-60' : 'max-w-14'
+            )}
+          >
+            <MenuList
+              showText={expanded}
+              library={libraryType}
+              variant="basic"
+              menuArrays={menuArrays}
+              type={menuType}
+              linkClassName="w-full"
+              className="w-full"
+              tooltipClassName="bg-white"
+            />
+          </div>
+        </div>
+        </div>
+        <div
+          className={cn(
+            'w-full mb-6 flex items-center justify-center px-4 py-4 mt-5 text-gray-600 transition-colors duration-300 transform dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700',
             expanded ? 'max-w-60' : 'max-w-16'
           )}
         >
@@ -118,7 +130,6 @@ export const SidebarThree = ({
             <ArrowRight className={cn('w-6 h-6')} />
           </button>
         </div>
-      </div>
       </div>
     </aside>
   );
